17788f52154dba8e6ab635d095f67d45f58c625a
Aluno SENAI <Aluno@SN-405093.edu.fiesc.com.br>
2026-06-02 15:45:16 -0300

    Criar personagem

---
+++ personagem.h
@@ -0,0 +1,20 @@
+# pragma once
+# include <iostream>
+# include <string>
+
+
+class Personagem { 
+public:
+	std::string nome;
+	int vida;
+	int ataque;
+	
+	Personagem(std::string nomePersonagem, int vidaPersonagem, int ataquePersonagem) 
+		: nome(nomePersonagem), vida(vidaPersonagem), ataque(ataquePersonagem) {}
+		
+	void apresentar () {
+		std:cout<< "Personagem: " << nome << std::end;
+		std:cout<< "Vida: " << vida << std::endl;
+		std:cout<< "Ataque: " << ataque << std::edl;	}
+	
+};
